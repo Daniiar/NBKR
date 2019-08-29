08bb37f360d1ed4b0dc43f47a22e5d4008f04d7b
Daniiar <lakiwolf.90@mail.ru>
2019-08-30 02:20:58 +0600

    added tests for logs

--- spec/nbkr/nbkr_spec.rb
+++ spec/nbkr/nbkr_spec.rb
@@ -4,28 +4,42 @@ require 'rails_helper.rb'
 
 RSpec.describe 'NBKR' do
   it 'hash_rates' do
-    expect(NBKR.hash_rates).to be_kind_of Hash
-    expect(NBKR.hash_rates).not_to be_empty
-    expect(NBKR.hash_rates).not_to be_nil
+    hash = NBKR.hash_rates
+
+    expect(hash).to be_kind_of Hash
+    expect(hash).not_to be_empty
+    expect(hash).not_to be_nil
   end
 
   it 'execute_request(create_record and update_record)' do
     current_hash = { 'USD' => '69,8490', 'EUR' => '77,3578', 'KZT' => '0,1803', 'RUB' => '1,0465' }
     NBKR.execute_request(current_hash)
 
-    expect(Rate.last.created_date).to eq(Date.today)
-    expect(Rate.last.currency).to eq('RUB')
-    expect(Rate.last.exchange_rate.to_s).to eq('1.0465')
+    last_record = Rate.last
+
+    expect(last_record.created_date).to eq(Date.today)
+    expect(last_record.currency).to eq('RUB')
+    expect(last_record.exchange_rate.to_s).to eq('1.0465')
 
     current_hash = { 'USD' => '70,1111', 'EUR' => '78,2222', 'KZT' => '1,3333', 'RUB' => '2,4444' }
     NBKR.execute_request(current_hash)
 
-    expect(Rate.last.created_date).to eq(Date.today)
-    expect(Rate.last.currency).to eq('RUB')
-    expect(Rate.last.exchange_rate.to_s).to eq('2.4444')
-  end
+    last_record = Rate.last
 
+    expect(last_record.created_date).to eq(Date.today)
+    expect(last_record.currency).to eq('RUB')
+    expect(last_record.exchange_rate.to_s).to eq('2.4444')
+  end
+end
+
+RSpec.describe 'NBKR logs' do
   it 'write logs' do
-    expect(File.read('log/nbkr.log').include?('https://www.nbkr.kg/XML/daily.xml 2019-08-29'))
+    logs_text = File.read('log/nbkr.log')
+    time_of_last_line = Time.parse(logs_text[-97..-73])
+    time_diference = Time.now - time_of_last_line
+
+    expect(logs_text.include?('https://www.nbkr.kg/XML/daily.xml 2019-08-29')).to eq(true)
+    expect(logs_text.include?(NBKR.hash_rates.to_s)).to eq(true)
+    expect(time_diference < 60).to eq(true)
   end
 end
