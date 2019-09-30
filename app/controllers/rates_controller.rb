4e28604a5bde92aa3196019e0dbf472009a3d403
Daniiar <lakiwolf.90@mail.ru>
2019-10-01 01:18:04 +0600

    added filter by dates and currency

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -4,4 +4,15 @@ class RatesController < ApplicationController
   def index
     @rates = Rate.all
   end
+
+  def search
+    @currency = params['currency']
+
+    start_date = RateHelper.parse_date(params['start_date'])
+    end_date = RateHelper.parse_date(params['end_date'])
+    range = { created_date: start_date..end_date }
+    sorted = Rate.all.where(range)
+
+    @sorted = sorted.where(currency: @currency)
+  end
 end
--- app/helpers/rate_helper.rb
+++ app/helpers/rate_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module RateHelper
+  def self.parse_date(date_params)
+    date_params = date_params.transform_values(&:to_i)
+
+    year = date_params['(1i)']
+    month = date_params['(2i)']
+    day = date_params['(3i)']
+
+    Date.new(year, month, day)
+  end
 end
