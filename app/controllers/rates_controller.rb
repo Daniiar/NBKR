b443c42c110a9502eab3bff91eeb4c48dba0a0ed
Daniiar <lakiwolf.90@mail.ru>
2019-10-07 23:54:31 +0600

    rename action search

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 class RatesController < ApplicationController
-  def index
-    @rates = Rate.all
+  def main_page
+    @quantity_of_records = Rate.count
+    @rates = Rate.last(8)
   end
 
-  def search
+  def search_rates_by_dates_and_currency
     @currency = params['currency']
 
     start_date = RateHelper.parse_date(params['start_date'])
     end_date = RateHelper.parse_date(params['end_date'])
     range = { created_date: start_date..end_date }
-    sorted = Rate.all.where(range)
+    sorted = Rate.where(range)
 
     @sorted = sorted.where(currency: @currency)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'search', to: 'rates#search'
-  root 'rates#index'
+  root 'rates#main_page'
+  get 'search_rates_by_dates_and_currency', to: 'rates#search_rates_by_dates_and_currency'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
